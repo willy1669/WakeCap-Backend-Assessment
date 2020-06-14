72a810be0ead9dd7687defc7f3f22d519b8f3137
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-14 21:54:49 +0100

    export all controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,15 @@
 import clientController from './client';
 import helmetLocationConroller from './helmetLocation';
 import helmetConroller from './helmet';
+import siteController from './site';
+import workerController from './worker';
+import activityController from './activity';
 
-export default { clientController, helmetLocationConroller, helmetConroller };
+export default {
+  clientController,
+  helmetLocationConroller,
+  helmetConroller,
+  siteController,
+  workerController,
+  activityController,
+};

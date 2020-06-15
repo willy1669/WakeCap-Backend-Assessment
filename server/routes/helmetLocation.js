372a685b1584bd8b210a92b1e1bc02b118887d76
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-15 14:26:44 +0100

    create routes for helmetLocation

--- server/routes/helmetLocation.js
+++ server/routes/helmetLocation.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import { CronJob } from 'cron';
-import controllers from '../controllers/helmetLocation';
+import controllers from '../controllers';
 
 const helmetLocation = express.Router();
 
 const {
-  helmetLocationController: { createHelmetLocation, updateHelmetLocation },
+  helmetLocationConroller: { updateHelmetLocation },
 } = controllers;
-
-/** create helmet location update from ip of helmet */
-helmetLocation.post('/register', createHelmetLocation);
 
 /** Cron that gets new location update from our assets every 3 minutes */
 const getAssetLocationUpdate = new CronJob('* 3 * * * *', updateHelmetLocation);
